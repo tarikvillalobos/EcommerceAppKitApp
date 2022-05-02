b6200d6a55720ef5b3c6ed6489ab189238b6596a
Tarik Villalobos <tk.villalobos@hotmail.com>
2022-05-02 18:02:46 -0300

    Initial Commit

---
+++ EcommerceAppKitApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EcommerceAppKitApp
+//
+//  Created by Tarik Adrien Villalobos on 05/06/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EcommerceAppKitApp/EcommerceAppKitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppKitAppApp.swift
+//  EcommerceAppKitApp
+//
+//  Created by Tarik Adrien Villalobos on 05/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppKitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
